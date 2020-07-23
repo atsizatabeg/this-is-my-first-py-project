a37734a87abb65c908caf3674aa56555331aa155
Enes Alper K <alperkaragoz49@gmail.com>
2020-07-23 05:34:21 +0300

    add py

---
+++ python.py
@@ -0,0 +1,10 @@
+first = "naber lan tarık"
+second = f"şş {first}"
+print(second)
+
+third = "py öğreniyorum"
+fourth = "yavaş yavaş {}"
+print(fourth.format(third))
+
+five = ("ben atsizatabeg bu benim ilk py projem")
+print(five)
