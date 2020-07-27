bfb397199706222bb07d4fc15a44296b83eccca1
Enes Alper K <alperkaragoz49@gmail.com>
2020-07-27 16:48:48 +0300

    add numbers file

---
+++ numbers.py
@@ -0,0 +1,12 @@
+x = 17 # int
+y = 17.6 # float
+z = "17" # string (str)
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(6/3) # ondalı böler
+print(6//3) # tam böler
+print(6**3) # üstü çarpar
+print(21%5) # bölmeden kalan
+print(6+3*4+6) # toplu işlem yapma
